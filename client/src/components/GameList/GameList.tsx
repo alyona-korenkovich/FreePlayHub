ae7774163ccefa9c4fb2c19fa714fdb996b21df2
Alyona Korenkovich <al.korenkovich@mail.ru>
2023-08-28 16:42:11 +0300

    #6 Added `GAMES_PER_PAGE` limit. The game list now only displays 10 games at first, then automatically displays +10 more games when the user reaches the end of the page.

--- client/src/components/GameList/GameList.tsx
+++ client/src/components/GameList/GameList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './GameList.module.scss';
 import { Heading, Text } from '@chakra-ui/react';
 import GameCard from '../GameCard/GameCard';
@@ -7,9 +7,37 @@ import GameFilter from '../GameFilter/GameFilter';
 import { TFetchGamesParams } from '../../types/TFetchGamesParams';
 import GameSorter from '../GameSorter/GameSorter';
 import Loader from '../Loader/Loader';
+import { TGame } from '../../types/TGame';
+import { GAMES_PER_PAGE } from '../../config/const';
+
 const GameList = () => {
+    const endOfListRef: React.RefObject<HTMLDivElement> = useRef(null);
+    const [visibleGames, setVisibleGames] = useState<TGame[]>([]);
     const [params, setParams] = useState<TFetchGamesParams>({});
     const [noResults, setNoResults] = useState<boolean>(false);
+
+    const expandGameList = () => {
+        if (games && Array.isArray(games)) {
+            setVisibleGames((prevState) => {
+                const newGamesToLoad = games.slice(
+                    prevState.length,
+                    prevState.length + GAMES_PER_PAGE,
+                );
+                const updatedState = prevState.concat(newGamesToLoad);
+                return updatedState;
+            });
+        }
+    };
+
+    const handleScroll = () => {
+        if (
+            endOfListRef.current &&
+            window.innerHeight + window.scrollY >=
+                endOfListRef.current.offsetTop
+        ) {
+            expandGameList();
+        }
+    };
 
     const {
         data: games,
@@ -19,16 +47,23 @@ const GameList = () => {
     } = gameAPI.useFetchGamesQuery(params);
 
     useEffect(() => {
+        setVisibleGames([]);
         refetch();
     }, [params]);
 
     useEffect(() => {
         if (games && Array.isArray(games)) {
+            expandGameList();
             setNoResults(false);
         } else if (games && typeof games === 'object' && games.status === 0) {
             setNoResults(true);
         }
     }, [games]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isLoading]);
 
     return (
         <main className={styles.list_container}>
@@ -72,14 +107,14 @@ const GameList = () => {
                             Произошла ошибка при загрузке данных
                         </Text>
                     )}
-                    {games &&
-                        Array.isArray(games) &&
-                        games.map((game) => (
+                    {visibleGames &&
+                        visibleGames.map((game) => (
                             <GameCard
                                 key={game.id}
                                 {...game}
                             />
                         ))}
+                    <div ref={endOfListRef} />
                 </div>
             </section>
         </main>
